48a8d46ec801d9553c1dd6391a2503a4e1ea0c1d
aiscarf <2428815343@qq.com>
2022-02-19 15:14:36 +0800

    feat: 重新实现批处理命令, 支持相对路径了.

--- Unity/Hotfix/CopyHotfix.bat
+++ Unity/Hotfix/CopyHotfix.bat
@@ -1,4 +1,15 @@
-@echo off
-copy ..\Temp\Bin\Debug\Hotfix.dll ..\Assets\Bundles\Code\Hotfix.dll.bytes
-copy ..\Temp\Bin\Debug\Hotfix.pdb ..\Assets\Bundles\Code\Hotfix.pdb.bytes
+@echo Sync Hotfix
+cd /d %~dp0
+
+set SourceDebugDir=%cd%\..\Temp\Bin\Debug
+set UnityCodePath=%cd%\..\Assets\Bundles\Code
+set ToolPath=%cd%\..\Tools\Pdb2Mdb\4.5
+
+copy %SourceDebugDir%\Hotfix.dll %UnityCodePath%\Hotfix.dll.bytes
+copy %SourceDebugDir%\Hotfix.pdb %UnityCodePath%\Hotfix.pdb.bytes
+copy %SourceDebugDir%\Hotfix.dll %ToolPath%\Hotfix.dll
+copy %SourceDebugDir%\Hotfix.pdb %ToolPath%\Hotfix.pdb
+
+%ToolPath%\pdb2mdb.bat
+
 pause
--- Unity/Tools/Pdb2Mdb/4.5/pdb2mdb.bat
+++ Unity/Tools/Pdb2Mdb/4.5/pdb2mdb.bat
@@ -1,4 +1,6 @@
-@echo off
+@echo Generate mdb.dll
+cd /d %~dp0
+set UnityCodePath=%cd%\..\..\..\Assets\Bundles\Code
 D:\Unity\2020.3.3f1c1\Unity\Editor\Data\MonoBleedingEdge\bin\mono.exe .\pdb2mdb.exe Hotfix.dll
-echo finish... 
+copy .\Hotfix.dll.mdb %UnityCodePath%\Hotfix.mdb.bytes
 pause
